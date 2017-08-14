2ba615ebeca86dba588d5688bf3c20da4ba21dcc
Arum <arum.galadima@gmail.com>
2017-08-14 07:13:38 -0400

    tag portion
    tag portion

--- proj3/js/script.js
+++ proj3/js/script.js
@@ -1,14 +1,30 @@
 //focus on the name field when the page is loaded
 document.getElementById('name').focus();
 
-//showing a text field when 'other' is selected
+//when the select element option is changed
 $('#title').change(function() {
-	const input = document.createElement('input');
+	//if the selected option has a value of 'other'
 	if ($(this).val() === 'other') {
+		//create a new input element
+		const input = document.createElement('input');
+		//set the id and placeholder attributes
 		$(input).attr('id', 'other-title');
 		$(input).attr('placeholder', 'Your Job Role');
+		//add the created input to the 'Job Role' section
 		$(this).after(input);
+	//if another section is selected hide the input
 	} else {
 		$("#other-title").hide();
+	}
+});
+
+$('#design').change(function () {
+	if($(this).val() === 'js puns') {
+		//$('#color').hide();
+		if ($('#color').val() === 'tomato' ||
+			$('#color').val() === 'steelblue' ||
+			$('#color').val() === 'dimgrey') {
+			$('#color').hide();
+		}
 	}
 });
